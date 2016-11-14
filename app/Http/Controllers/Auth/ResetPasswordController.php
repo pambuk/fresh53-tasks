cc0f611e4dbd643a16ac1a853c2bb0a4189824df
pambuk <wojtek.zymonik@gmail.com>
2016-11-14 10:48:47 +0100

    Reset token with password reset

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -1,9 +1,10 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Str;
 
 class ResetPasswordController extends Controller
 {
@@ -35,5 +36,23 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    /**
+     * Reset the given user's password.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string  $password
+     * @return void
+     */
+    protected function resetPassword($user, $password)
+    {
+        $user->forceFill([
+            'password' => bcrypt($password),
+            'remember_token' => Str::random(60),
+            'api_token' => Str::random(60),
+        ])->save();
+
+        $this->guard()->login($user);
     }
 }
---
+++ resources/assets/js/store/mutation-types.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Wojtek on 2016-11-14.
+ */
